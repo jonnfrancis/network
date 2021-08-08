6b194abbfbaa5e9ff5ffd98aeeebd8e4159fc44e
John Francis <johnmosima7@gmail.com>
2021-08-08 10:11:52 +0300

    fixed like

--- network/views.py
+++ network/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, Post, Profile
+
+import json
 
 
 def index(request):
@@ -61,3 +66,85 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+@login_required
+def load_followed_posts(request):
+    followed_profiles = request.user.get_followed_profiles.all()
+    print(followed_profiles)
+    posts = Post.objects.filter(creator__in=followed_profiles).all()
+    return paginated_posts(request,posts)
+
+
+def profile(request,user_id):
+    profile = Profile.objects.filter(id=user_id).first()
+    return JsonResponse(profile.serialize(request.user), status=200)
+
+
+def load_posts(request):
+    profile = request.GET.get("profile", None)
+    if (profile):
+        posts = Post.objects.filter(creator=profile).all()
+    else:
+        posts = Post.objects.all()
+    return paginated_posts(request,posts)      
+
+def paginated_posts(request,posts):
+    posts = posts.order_by("-created_date").all()
+    paginator = Paginator(posts,10)
+    page_obj = paginator.get_page(request.GET['page']) 
+    return JsonResponse({
+        "posts" : [post.serialize(request.user) for post in page_obj],
+        "num_pages" : paginator.num_pages
+    }
+    , safe=False)    
+
+@login_required
+def save_post(request):
+    if request.method == "POST":
+        form = Post(content=request.POST['content'])
+        form.creator = Profile.objects.get(user=request.user)
+        form.save()
+    elif request.method =="PUT":
+        data = json.loads(request.body)
+        post_id= int(data["post_id"])
+        new_content = data["new_content"]
+        post = Post.objects.filter(id=post_id).first()
+        if post.creator.user != request.user:
+            return HttpResponse(status=401)
+        post.content = new_content
+        post.save()
+        return JsonResponse({
+            "result": True
+        }, status=200)
+    else:
+        return JsonResponse({
+            "error": f"request methods supported: POST, GET"
+        }, status=400)
+    return index(request) 
+
+@login_required
+def update_follow(request,profile_id):
+    profile = Profile.objects.get(id=profile_id)
+    if profile in request.user.get_followed_profiles.all():
+        newStatus = False
+        profile.followers.remove(request.user)
+    else:
+        newStatus = True
+        profile.followers.add(request.user)
+    profile.save()
+    return JsonResponse({"newFollower": newStatus, "newAmount": profile.followers.count()},status=200)
+
+@login_required
+def update_like(request,post_id):
+    profile = Profile.objects.filter(user=request.user).first()
+    post = Post.objects.get(id=post_id)
+    if post in profile.get_all_liked_posts.all():
+        newStatus = False
+        post.likes.remove(profile) 
+    else:
+        newStatus = True
+        post.likes.add(profile)
+    post.save()
+    return JsonResponse({"liked": newStatus, "newAmount": post.likes.count()}, status=200) 
+
+                                             
